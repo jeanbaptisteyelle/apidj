4995142cba6dcd0db07316b9ddfa57d72a3d9db7
jeanbaptisteyelle <yellejeanbaptiste@gmail.com>
2020-09-10 11:25:27 +0000

    ajout ligne whitenoise

--- DJ/settings.py
+++ DJ/settings.py
@@ -53,9 +53,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'DJ.urls'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES = [
     {
